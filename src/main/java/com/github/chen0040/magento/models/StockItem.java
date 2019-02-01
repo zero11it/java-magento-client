ff4491513cb67123b889b090ad8c78c552d6036a
flavio <flavio.troia@stasbranger.com>
2019-02-01 13:26:32 +0100

    upgrade entity model StockItem added more fields

--- src/main/java/com/github/chen0040/magento/models/StockItem.java
+++ src/main/java/com/github/chen0040/magento/models/StockItem.java
@@ -2,6 +2,9 @@ package com.github.chen0040.magento.models;
 
 public class StockItem {
 
+    private long item_id;
+    private long product_id;
+    private long stock_id;
     private int qty;
     private boolean is_in_stock;
 
@@ -19,5 +22,29 @@ public class StockItem {
 
     public void setIs_in_stock(boolean is_in_stock) {
         this.is_in_stock = is_in_stock;
+    }
+
+    public long getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(long item_id) {
+        this.item_id = item_id;
+    }
+
+    public long getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(long product_id) {
+        this.product_id = product_id;
+    }
+
+    public long getStock_id() {
+        return stock_id;
+    }
+
+    public void setStock_id(long stock_id) {
+        this.stock_id = stock_id;
     }
 }
