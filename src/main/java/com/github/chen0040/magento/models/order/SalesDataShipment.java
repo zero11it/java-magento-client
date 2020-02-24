4ceab625813e53d8b1b19c6b5500ed6a0872e0ab
a.trucco <a.trucco@zero11.it>
2020-02-24 11:01:49 +0100

    Added helper methods for array fields of SalesDataShipment

--- src/main/java/com/github/chen0040/magento/models/order/SalesDataShipment.java
+++ src/main/java/com/github/chen0040/magento/models/order/SalesDataShipment.java
@@ -1,5 +1,8 @@
 package com.github.chen0040.magento.models.order;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import lombok.Getter;
@@ -17,4 +20,94 @@ public class SalesDataShipment {
 	private List<SalesDataTrack> tracks;
 	private List<SalesDataPackage> packages;
 	private ShipmentArguments arguments;
+	
+	//
+	// Items
+	//
+	public SalesDataShipment addItem(SalesDataItem item) {
+		if (this.items == null) {
+			this.items = new ArrayList<>();
+		}
+		
+		this.items.add(item);
+		return this;
+	}
+	
+	public SalesDataShipment addItems(SalesDataItem... items) {
+		if (this.items == null) {
+			this.items = new ArrayList<>();
+		}
+		
+		this.items.addAll(Arrays.asList(items));
+		return this;
+	}
+	
+	public SalesDataShipment addItems(Collection<SalesDataItem> items) {
+		if (this.items == null) {
+			this.items = new ArrayList<>();
+		}
+		
+		this.items.addAll(items);
+		return this;
+	}
+	
+	//
+	// Tracks
+	//
+	public SalesDataShipment addTrack(SalesDataTrack track) {
+		if (this.tracks == null) {
+			this.tracks = new ArrayList<>();
+		}
+		
+		this.tracks.add(track);
+		return this;
+	}
+	
+	public SalesDataShipment addTracks(SalesDataTrack... tracks) {
+		if (this.tracks == null) {
+			this.tracks = new ArrayList<>();
+		}
+		
+		this.tracks.addAll(Arrays.asList(tracks));
+		return this;
+	}
+	
+	public SalesDataShipment addTracks(Collection<SalesDataTrack> tracks) {
+		if (this.tracks == null) {
+			this.tracks = new ArrayList<>();
+		}
+		
+		this.tracks.addAll(tracks);
+		return this;
+	}
+	
+	//
+	// Packages
+	//
+	public SalesDataShipment addPackage(SalesDataPackage _package) {
+		if (this.packages == null) {
+			this.packages = new ArrayList<>();
+		}
+		
+		this.packages.add(_package);
+		return this;
+	}
+	
+	public SalesDataShipment addPackages(SalesDataPackage... packages) {
+		if (this.packages == null) {
+			this.packages = new ArrayList<>();
+		}
+		
+		this.packages.addAll(Arrays.asList(packages));
+		return this;
+	}
+	
+	public SalesDataShipment addPackages(Collection<SalesDataPackage> packages) {
+		if (this.packages == null) {
+			this.packages = new ArrayList<>();
+		}
+		
+		this.packages.addAll(packages);
+		return this;
+	}
 }
