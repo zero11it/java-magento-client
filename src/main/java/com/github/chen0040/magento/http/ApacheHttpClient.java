1f495766480ba4e06642554d626037dca1c8ed5a
James Dumay <james@dadndavesbrewing.com>
2020-03-10 13:19:09 +1100

    Introduce configureClient() to ApacheHttpClient

--- src/main/java/com/github/chen0040/magento/http/ApacheHttpClient.java
+++ src/main/java/com/github/chen0040/magento/http/ApacheHttpClient.java
@@ -5,31 +5,18 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.Map;
 
-/**
- * Created by xschen on 3/10/16.
- */
 public class ApacheHttpClient implements HttpClient {
 	private static final String DATA_ENCODING = "UTF-8";
 
-	private static CloseableHttpClient buildClient() {
-		int timeout = 60;
-		RequestConfig config = RequestConfig.custom()
-				.setSocketTimeout(timeout * 1000)
-				.setConnectionRequestTimeout(timeout * 1000)
-				.setConnectTimeout(timeout * 1000)
-				.build();
-
-		return HttpClients.custom().setDefaultRequestConfig(config).build(); // builder.build();
-	}
-
 	public String jsonPost(final String url, Map<String, String> parameters) throws IOException {
-		try (CloseableHttpClient httpClient = buildClient()) {
+		try (CloseableHttpClient httpClient = configureClient().build()) {
 			HttpPost request = new HttpPost(url);
 			String body = JSON.toJSONString(parameters);
 			StringEntity params = new StringEntity(body);
@@ -42,7 +29,7 @@ public class ApacheHttpClient implements HttpClient {
 	}
 
 	public String post(final String url, String body, Map<String, String> headers) throws IOException {
-		try (CloseableHttpClient httpClient = buildClient()) {
+		try (CloseableHttpClient httpClient = configureClient().build()) {
 			HttpPost request = new HttpPost(url);
 			StringEntity params = new StringEntity(body);
 
@@ -57,7 +44,7 @@ public class ApacheHttpClient implements HttpClient {
 	}
 
 	public String put(final String url, String body, Map<String, String> headers) throws IOException {
-		try (CloseableHttpClient httpClient = buildClient()) {
+		try (CloseableHttpClient httpClient = configureClient().build()) {
 			HttpPut request = new HttpPut(url);
 			StringEntity params = new StringEntity(body);
 
@@ -72,7 +59,7 @@ public class ApacheHttpClient implements HttpClient {
 	}
 
 	public String delete(final String url, final Map<String, String> headers) throws IOException {
-		try (CloseableHttpClient httpClient = buildClient()) {
+		try (CloseableHttpClient httpClient = configureClient().build()) {
 			HttpDelete request = new HttpDelete(url);
 			for (Map.Entry<String, String> entry : headers.entrySet()) {
 				request.addHeader(entry.getKey(), entry.getValue());
@@ -84,7 +71,7 @@ public class ApacheHttpClient implements HttpClient {
 	}
 
 	public String get(final String url, final Map<String, String> headers) throws IOException {
-		try (CloseableHttpClient httpClient = buildClient()) {
+		try (CloseableHttpClient httpClient = configureClient().build()) {
 			HttpGet request = new HttpGet(url);
 			for (Map.Entry<String, String> entry : headers.entrySet()) {
 				request.addHeader(entry.getKey(), entry.getValue());
@@ -93,5 +80,16 @@ public class ApacheHttpClient implements HttpClient {
 				return result.getEntity() != null ? EntityUtils.toString(result.getEntity(), DATA_ENCODING) : "";
 			}
 		}
+	}
+
+	protected HttpClientBuilder configureClient() {
+		int timeout = 60;
+		RequestConfig config = RequestConfig.custom()
+				.setSocketTimeout(timeout * 1000)
+				.setConnectionRequestTimeout(timeout * 1000)
+				.setConnectTimeout(timeout * 1000)
+				.build();
+
+		return HttpClients.custom().setDefaultRequestConfig(config);
 	}
 }
